8e20ced513d35ce6446a659d5dbe7e6234af6720
yusufozgur <yusufozgurtr@gmail.com>
2024-08-27 15:32:15 +0300

    9

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.DEPLOY_URL ? process.env.DEPLOY_URL : "http://localhost:3000",
+    baseURL: process.env.DATABASE_URL ? process.env.DATABASE_URL : "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
